b797f55bf0c79b633341d245b32140206d29ae98
Adrian333Dev <adrian333dev@gmail.com>
2023-01-05 06:01:32 +0400

    Section 7: MVC completed

--- first-app/app.js
+++ first-app/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const { notFoundPage } = require('./controllers/errors');
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -14,11 +16,9 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminData);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(notFoundPage);
 
 app.listen(3000);
